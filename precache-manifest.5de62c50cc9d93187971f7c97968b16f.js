6055649c7713eeb7a075cba4eb5fde0e14b51f69
dzucconi <mail@damonzucconi.com>
2019-07-29 17:14:20 -0400

    Updates

---
+++ precache-manifest.5de62c50cc9d93187971f7c97968b16f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9539137f3436c4cc43bc3f8440643874",
+    "url": "/moda-icons/index.html"
+  },
+  {
+    "revision": "e3c6b5fd6b030091359a",
+    "url": "/moda-icons/static/css/main.7f8aacfb.chunk.css"
+  },
+  {
+    "revision": "64b48ed2264f98d3f6a1",
+    "url": "/moda-icons/static/js/2.5b52474e.chunk.js"
+  },
+  {
+    "revision": "e3c6b5fd6b030091359a",
+    "url": "/moda-icons/static/js/main.83ea2a81.chunk.js"
+  },
+  {
+    "revision": "060818bad09bcb9f9cc1",
+    "url": "/moda-icons/static/js/runtime~main.9885c4d3.js"
+  }
+]);
